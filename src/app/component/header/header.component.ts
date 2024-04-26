110abc29dc8a6c763fba3f8e45c8c3bd6614b41d
Prateek <prateek.upadhayay_pnm@ksolves.com>
2024-04-26 18:48:01 +0530

    initial commit

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import { RegistrationComponent } from '../registration/registration.component';
 
 @Component({
   selector: 'app-header',
@@ -10,5 +12,16 @@ import { MatMenuModule } from '@angular/material/menu';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(public dialog: MatDialog) {}
+
+  // registerUser() {
+  //   const dialogRef = this.dialog.open(RegistrationComponent, {
+  //     width: '600',
+  //   });
+
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     console.log(`Dialog result: ${result}`);
+  //   });
+  // }
 
 }
